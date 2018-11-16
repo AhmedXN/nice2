986ed7e0e93a2c8aaa14248aa8465fa220205146
AhmedXN <44243584+AhmedXN@users.noreply.github.com>
2018-11-16 17:46:37 +0300

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = "-"
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '410559969193361409') return;
+
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+
+hero.login(process.env.token)
