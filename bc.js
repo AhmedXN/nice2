73828b0b940a75a818cb595ef64e8ffb7611b1df
AhmedXN <44243584+AhmedXN@users.noreply.github.com>
2018-11-17 21:33:48 +0300

    Update bc.js

--- bc.js
+++ bc.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "c";
 var adminprefix = 'c'
 
-const developers = ["472413769700474901","id"]
+const developers = ["472413769700474901"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
